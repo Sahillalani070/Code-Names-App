e05510174dc3b86152e9f32a3ca24efa9d209103
Sahil Lalani <69220306+Sahillalani070@users.noreply.github.com>
2023-10-26 16:20:58 -0400

    Cleared the clutter from important files like App.js and Game.js and moved basic functions to helper files.

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
-import Game from './Game';
-import OnBoarding from './OnBoarding';
-import Helper from './GameHelper';
 import { Amplify } from 'aws-amplify';
 import config from "./src/aws-exports";
 import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
-
+import Nav from './Navbar';
 
 Amplify.configure(config);
 
-const Drawer = createDrawerNavigator();
 function App() {
   return (
-    // <View style={styles.container}>
-    //   {/* <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" /> */}
-    // </View>
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Buttontest" options={{ headerShown: false }}>
-        {/* <Drawer.Screen name="Login" component={Login} options={{ headerShown: false }} /> */}
-        <Drawer.Screen name="Buttontest" component={OnBoarding} options={{ headerShown: false }} />
-        <Drawer.Screen name="Game" component={Game} options={{ headerShown: false }} />
-        <Drawer.Screen name="GameHelper" component={Helper} options={{ headerShown: false }} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    Nav()
   );
 }
 
@@ -35,3 +16,4 @@ const styles = StyleSheet.create({
 
 });
 export default withAuthenticator(App);
+
--- GameHelper.js
+++ GameHelper.js
@@ -5,13 +5,139 @@ import { StatusBar } from "expo-status-bar";
 import Icon from 'react-native-ico-material-design';
 import { useNavigation } from "@react-navigation/native";
 
-const Helper = () => {
+// extracting data
+const DataList = jsonData.words;
+var value = IndicestoWords();
+
+// Generate Random indicies
+function RandomIndices() {
+    var x = Math.floor(Math.random() * DataList.length)
+    var list1 = [x];
+    for (var i = 1; i < 25; i++) {
+        while (list1.includes(x)) {
+            x = Math.floor(Math.random() * DataList.length)
+        }
+        list1.push(x);
+        // console.log(x);
+    }
+    return (list1);
+}
+
+function IndicestoWords() {
+    var listi = RandomIndices();
+    var listw = [""];
+    listw.pop();
+    var n = 0;
+    while (n < 25) {
+        listw.push(DataList[listi[n]]);
+        n++;
+    }
+    // console.log(listw)
+    return listw;
+}
+const Boxes = () => {
     return (
-        <View>
-            <TouchableOpacity onPress={() => console.log("help")}>
-                <Text>Road</Text>
-            </TouchableOpacity>
+        <View style={styles.Screen}>
+            <View style={styles.boxContainer}>
+                {
+                    value.map((item, index) => (
+                        <TouchableOpacity key={index} style={styles.box} onPress={() => { console.log(index); }}>
+                            <View style={styles.inner}>
+                                <TouchableOpacity style={styles.IconBehave} onPress={() => { console.log("key"); }}>
+                                    <Icon name="thumb-up-button" style={{ color: '#448aff' }} height="12" />
+                                </TouchableOpacity>
+                                <Text style={styles.styletext}>{item}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                    )}
+            </View>
+            <View style={styles.Cluebox}>
+                <TextInput style={{ height: "50%", width: "50%", borderWidth: 1, borderRadius: 10, alignSelf: "center" }}
+                    placeholder="Type here..."
+                />
+
+            </View>
+            <View style={styles.boxContainer2}>
+                <View style={styles.teambox} backgroundColor="rgb(236,91,32)">
+                    <Text>Red Team</Text>
+                </View>
+                <View style={styles.Gamelog} backgroundColor="rgb(242,215,180)">
+                </View>
+                <View style={styles.teambox} backgroundColor="rgb(2,172,197)">
+                    <Text>Blue Team</Text>
+                </View>
+            </View>
         </View>
     )
 }
-export default Helper;
+export default Boxes;
+const styles = StyleSheet.create({
+    boxContainer: {
+        width: '100%',
+        height: '40%',
+        //backgroundColor: 'red',
+        padding: 2,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    boxContainer2: {
+        width: '100%',
+        height: '100%',
+        //backgroundColor: 'red',
+        // padding: 2,
+        flexDirection: 'row',
+        // flexWrap: 'wrap',
+    },
+
+    box: {
+        width: '20%',
+        height: '20%',
+        padding: 2,
+        backgroundColor: 'green'
+    },
+    inner: {
+        flex: 1,
+        backgroundColor: '#c8c8c8',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10
+    },
+    IconBehave: {
+        alignSelf: "flex-end",
+        paddingRight: 2,
+        paddingBottom: 2,
+        fontSize: 10
+    },
+    styletext: {
+        paddingBottom: 15,
+        fontSize: 10
+    },
+    teambox: {
+        // paddingTop: 2,
+        height: '50%',
+        width: '35%',
+        // borderWidth: 2, // Border width in pixels
+        // borderColor: 'black', // Border color
+        // borderRadius: 10,
+    },
+    Gamelog: {
+        height: '50%',
+        width: '30%',
+        borderWidth: 2, // Border width in pixels
+        borderColor: 'black', // Border color
+        borderRadius: 10,
+        padding: 15
+    },
+    Cluebox: {
+        width: '100%',
+        height: '10%',
+        borderWidth: 2, // Border width in pixels
+        borderColor: 'black', // Border color
+        borderRadius: 10,
+    },
+    Screen: {
+        height: '100%',
+        width: '100%'
+    }
+});
